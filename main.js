cfe4d380236f6fd37581ef6686e9ecf6e41f709e
Rhishi kesh <reshikash300@gmail.com>
2023-12-20 17:08:13 +0600

    Cart Part Complete

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-const app = vue.createApp({
-    
+const app = Vue.createApp({
+    data: function(){
+        return {
+            variants: [
+                {id: 1, name: 'Vanilla', price: 100},
+                {id: 2, name: 'Chocolate', price: 150},
+                {id: 3, name: 'Strawberry', price: 50},
+                {id: 4, name: 'Orange', price: 70},
+                {id: 5, name: 'Lemon', price: 90},
+            ],
+            cart: [],
+        }
+    },
+    methods: {
+        addToCart(variantId) {
+            this.cart.push(this.variants.find(variant => variant.id === variantId))
+        },
+        removeFormCart(variantId) {
+            let position = this.cart.findIndex(variant => variant.id === variantId);
+            this.cart.splice(position, 1);
+        }
+    },
+    computed: {
+        total(){
+            return this.cart.reduce((t, variant) => t + variant.price, 0);
+        }
+    }
 });
